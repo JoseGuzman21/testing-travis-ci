dd7901dd74c0943e8b332fc6da5e562485e4324a
JoseGuzman21 <37311990+JoseGuzman21@users.noreply.github.com>
2020-11-30 22:03:38 -0500

    add v2

--- mymodule.py
+++ mymodule.py
@@ -1,7 +1,4 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 def sum(a, b):
     for n in (a, b):
-        if not isinstance(n, int) and not isinstance(n, float):
-            raise TypeError
+        if not isinstance(n, int) and not isinstance(n, float): raise TypeError
     return a + b
--- tests/test_module.py
+++ tests/test_module.py
@@ -4,30 +4,19 @@ import unittest
 import mymodule
 
 class TestMyModule(unittest.TestCase):
-
     def test_sum(self):
-        self.assertEqual(mymodule.sum(1, 2), 3)
-
-    def test_sum1(self):
         self.assertEqual(mymodule.sum(10, 20), 30)
 
+    def test_sum1(self):
+        self.assertEqual(mymodule.sum(0, 0), 0)
+
     def test_sum2(self):
-        self.assertEqual(mymodule.sum(100, 200), 300)
+        self.assertEqual(mymodule.sum(-1, 5), 4)
 
     def test_sum3(self):
-        self.assertEqual(mymodule.sum(500, 700), 1200)
+        self.assertEqual(mymodule.sum(-1, -4), -5)
 
     def test_sum4(self):
-        self.assertEqual(mymodule.sum(1000, 2000), 3000)
-
-    def test_sum5(self):
-        self.assertEqual(mymodule.sum(120020, 80), 120100)
-
-    def test_sum6(self):
-        self.assertEqual(mymodule.sum(1200000, 1200000), 2400000)
-
-    def test_sum7(self):
-        self.assertEqual(mymodule.sum(120000000, 120000000), 240000000)
-
-if __name__ == "__main__":
-    unittest.main()
+        self.assertEqual(mymodule.sum(10, 20), 30)
+#if __name__ == "__main__":
+#    unittest.main()
