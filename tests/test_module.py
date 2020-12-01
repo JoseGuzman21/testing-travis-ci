30504a86ee104afb1b81986623515b09c9c7c054
JoseGuzman21 <37311990+JoseGuzman21@users.noreply.github.com>
2020-11-30 20:53:15 -0500

    add more test

--- tests/test_module.py
+++ tests/test_module.py
@@ -6,7 +6,22 @@ import mymodule
 class TestMyModule(unittest.TestCase):
 
     def test_sum(self):
-        self.assertEqual(mymodule.sum(5, 7), 12)
+        self.assertEqual(mymodule.sum(1, 2), 3)
+
+    def test_sum1(self):
+        self.assertEqual(mymodule.sum(10, 20), 30)
+
+    def test_sum2(self):
+        self.assertEqual(mymodule.sum(100, 200), 300)
+
+    def test_sum3(self):
+        self.assertEqual(mymodule.sum(500, 700), 1200)
+
+    def test_sum4(self):
+        self.assertEqual(mymodule.sum(1000, 2000), 3000)
+
+    def test_sum5(self):
+        self.assertEqual(mymodule.sum(120020, 80), 120100)
 
 if __name__ == "__main__":
     unittest.main()
